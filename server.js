1a201301a6f41e48332db077737dc8025ed885ed
captainsloths <alex.eiselstein@gmail.com>
2020-12-10 22:19:14 -0600

    Removed leeway time in unix date

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.get("/api/hello", function (req, res) {
 });
 
 app.get("/api/timestamp", (req, res) => {
-  res.json({unix: Date.now(), utc: Date() - 3 });
+  res.json({unix: Date.now(), utc: Date()});
 });
 
 app.get("/api/timestamp/:date_string", (req,res) => {
